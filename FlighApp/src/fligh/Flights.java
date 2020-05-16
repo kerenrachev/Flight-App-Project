a1740d3720f22164c544a4db511ab43284ea1371
Eyal the Eyaly <Eyal the Eyaly@DESKTOP-0371MUF.lan>
2020-05-16 19:25:27 +0300

    test

--- FlighApp/src/fligh/Flights.java
+++ FlighApp/src/fligh/Flights.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Flights {
-
+//test
 	protected LocalDateTime dateTime;
 	protected boolean ifLanding=false;
 	protected int numOfFligh;
